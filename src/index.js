9c9d6ef188d782d105b5b499ced00e76ccab309f
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-08-16 23:41:44 +0430

    up

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+  
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
